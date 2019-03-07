4619850d0f0fa9082103297f6ab6bb4f098c7ef5
Arpana <arpanageorge@yahoo.in>
2019-03-06 23:21:45 -0800

    Completed Updated Files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # unit-4-game
+
+## Program logic:<br>
+->First called the function randomNubergeneration() which will <br>
+    - generate the random number which the user has to make by clicking the crystals and assigned it to variable randNumber<br>
+    - generate the random number for each crystals and set it as value attribute for each crystal<br>
+
+->Next when user clicks on each crystal, added the random number assigned to each crystal as value attribute and displayed it as your total<br>
+    -if(yourTotal>randNumber), incremented Losses and called fuction randomNubergeneration() inorder to generate new random numbers for next round of game and resetted var yourTotal<br>
+    -else if(yourTotal===randNumber), incremented wins and called fuction randomNubergeneration() inorder to generate new random numbers for next round of game and resetted var yourTotal to 0<br>
---
+++ assets/javascript/game.js
@@ -0,0 +1,85 @@
+//Variable declarations
+var randNumber, blueRandNumber, PinkRandNumber,YellowRandNumber,purpleRandNumber ;
+var yourTotal = 0;
+var wins=0, losses=0;
+
+$(document).ready(function(){
+    //Hiding You Win or Loss message
+    $(".WinLossStatus").hide();
+    //Calling random number generation function for generating random number 
+    randomNubergeneration();
+
+    //Adding crystal random number to your total when crystal is clicked
+    $(".crystal").click(function(){
+       //Hiding You Win or Loss message
+      $(".WinLossStatus").hide();
+      yourTotal+= parseInt($(this).attr("value"));
+      $(".total").html("Your Total"+"<br>"+yourTotal);
+      console.log("total"+yourTotal);
+      if(yourTotal>randNumber)
+       {
+            losses++;
+            $(".winLossNumber").html("Wins "+"&ensp;"+wins+"<br>"+"Losses "+"&ensp;"+losses);
+            $(".WinLossStatus").html("You Loss"+"<br>"+"Number was "+randNumber+" and Your total was "+yourTotal+"<br>"+ "Click crystals to play again");
+            $(".WinLossStatus").css("background-color", "red").slideDown("slow");
+            randomNubergeneration();
+            reset();
+        }
+        else if(yourTotal===randNumber)
+       {
+            wins++;
+            // console.log("wims"+wins);
+            $(".winLossNumber").html("Wins "+"&ensp;"+wins+"<br>"+"Losses "+"&ensp;"+losses);
+            $(".WinLossStatus").html("You Win "+"<br>"+"Number was "+randNumber+" and Your total was"+yourTotal+"<br>"+ "Click crystals to play again");
+            $(".WinLossStatus").css("background-color", "green").slideDown("slow");
+            randomNubergeneration();
+            reset();
+        }
+    });
+});
+
+//Function Definitions
+function randomNubergeneration()
+{
+    //Random number user has to guess
+    randNumber = getRndInteger(19, 120);
+    $(".randomNumber").text(randNumber);
+    // console.log("randnum"+randNumber);
+
+    //Random number for blue crystal
+    blueRandNumber = getRndInteger(1, 12);
+    console.log("bluecrystal randnum="+blueRandNumber);
+    $("#blueCrystal").attr("value",blueRandNumber);
+    // console.log($("#blueCrystal").attr("value"));
+    
+    //Random number for pink crystal
+    PinkRandNumber = getRndInteger(1, 12);
+    // console.log("pinkcrystal randnum="+PinkRandNumber);
+    $("#pinkCrystal").attr("value",PinkRandNumber);
+
+    //Random number for yellow crystal
+    YellowRandNumber = getRndInteger(1, 12);
+    // console.log("yellowcrystal randnum="+YellowRandNumber);
+    $("#yellowCrystal").attr("value",YellowRandNumber);
+
+    //Random number for green crystal
+    purpleRandNumber = getRndInteger(1, 12);
+    // console.log("greencrystal randnum="+purpleRandNumber);
+    $("#purpleCrystal").attr("value",purpleRandNumber);
+}
+
+
+//Random Number generation function
+function getRndInteger(min, max) 
+{
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+//Reset Function for resetting everything when one round of game is over
+function reset()
+{
+    yourTotal = 0;
+    $(".total").html("Your Total"+"<br>"+yourTotal);
+    //$(".WinLossStatus").hide();
+}
+
